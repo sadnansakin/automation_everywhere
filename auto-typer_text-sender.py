885de12e0dde385a8225d4a7ae697c5215ab6995
sadnansakin <66565192+sadnansakin@users.noreply.github.com>
2023-02-22 00:02:36 +0600

    Add files via upload

---
+++ auto-typer_text-sender.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import webbrowser
+
+time.sleep(3)
+
+print("Are you ready? Type: yes or no")
+take_input = input()
+
+time.sleep(1)
+
+if take_input == "yes":
+    print("please open web.whatsapp.com and and click on type a message box")
+    time.sleep(2)
+    while True:
+        pyautogui.typewrite("valo hoye jao tomra")
+        time.sleep(0.1)
+        pyautogui.press("enter")
+        time.sleep(0.1)
+
+elif take_input == "no":
+    #pyautogui.hotkey('ctrl','c')
+    quit() # quit() is for exit a python program.
+
+
+
